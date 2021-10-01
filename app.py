e9950c975e02f880b01c81c7d149a50694f5cd81
Hareesh Nayak <hrnayak8@gmail.com>
2021-10-01 10:52:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html", data="hi")
+
+@app.route('/prediction', methods=["POST"])
+def prediction():
+
+    return "wellcome to prediction"
+
+if __name__ == "__main__":
+    app.run(debug=True)
